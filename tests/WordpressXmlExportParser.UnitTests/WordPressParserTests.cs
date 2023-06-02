61c63d602e6c43f221fdfd93b5f3f1a95cd5a032
Mamo, Ruben <RubenMamo@kpmg.com.mt>
2023-06-02 19:08:47 +0200

    Added additional code coverage

---
+++ tests/WordpressXmlExportParser.UnitTests/WordPressParserTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressXmlExportParser.UnitTests
+{
+    [TestFixture]
+    public class WordPressParserTests
+    {
+        [Test]
+        public void ParsesXml()
+        {
+			var blog = WordPressParser.ReadXml(XDocument.Parse(xml));
+
+			blog.Should().NotBeNull();
+        }
+
+        private string xml = @"<?xml version=""1.0"" encoding=""UTF-8"" ?>
+<rss
+	xmlns:excerpt=""http://wordpress.org/export/1.2/excerpt/""
+	xmlns:content=""http://purl.org/rss/1.0/modules/content/""
+	xmlns:wfw=""http://wellformedweb.org/CommentAPI/""
+	xmlns:dc=""http://purl.org/dc/elements/1.1/""
+	xmlns:wp=""http://wordpress.org/export/1.2/""
+>
+<channel>
+	<title>Some Blog</title>
+	<link>https://wordpressxmlexportparser.local</link>
+	<description>Some blog description</description>
+	<pubDate>Tue, 30 May 2023 07:22:20 +0000</pubDate>
+	<language>en-US</language>
+	<wp:wxr_version>1.2</wp:wxr_version>
+	<wp:base_site_url>https://wordpressxmlexportparser.local</wp:base_site_url>
+	<wp:base_blog_url>https://wordpressxmlexportparser.local/blog</wp:base_blog_url>
+</channel>
+</rss>";
+    }
+}
