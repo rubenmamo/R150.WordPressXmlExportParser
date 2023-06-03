a1c2d70c64bdec230074710db4e2fc53f2dfa590
Mamo, Ruben <RubenMamo@kpmg.com.mt>
2023-06-03 09:58:40 +0200

    Added support for categories

---
+++ src/WordpressXmlExportParser/CategoryExtractor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WordPressXmlExportParser
+{
+    /*		<wp:category>
+		<wp:term_id>2</wp:term_id>
+		<wp:category_nicename><![CDATA[blogging]]></wp:category_nicename>
+		<wp:category_parent><![CDATA[]]></wp:category_parent>
+		<wp:cat_name><![CDATA[Blogging]]></wp:cat_name>
+	</wp:category>*/
+    internal class CategoryExtractor
+    {
+        internal static ReadOnlyCollection<Category> ExtractCategories(XElement channelElement)
+        {
+            return new ReadOnlyCollection<Category>(channelElement
+                .Elements(WordPressParser.WordPressXmlNamespace + "category")
+                .Select(ExtractCategory)
+                .ToList());
+        }
+
+        private static Category ExtractCategory(XElement categoryElement)
+        {
+            var termIdElement = categoryElement.Element(WordPressParser.WordPressXmlNamespace + "term_id");
+            var niceNameElement = categoryElement.Element(WordPressParser.WordPressXmlNamespace + "category_nicename");
+            var parentCategoryElement = categoryElement.Element(WordPressParser.WordPressXmlNamespace + "category_parent");
+            var nameElement = categoryElement.Element(WordPressParser.WordPressXmlNamespace + "cat_name");
+
+            return new Category 
+            { 
+                Id = int.Parse(termIdElement.Value),
+                NiceName = niceNameElement.Value,
+                ParentCategoryName = parentCategoryElement.Value,
+                Name = nameElement.Value
+            };
+        }
+    }
+}
--- src/WordpressXmlExportParser/WordPressBlog.cs
+++ src/WordpressXmlExportParser/WordPressBlog.cs
@@ -1,20 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WordPressXmlExportParser
 {
     public class WordPressBlog
     {
-		public string Title { get; set; }
+		public string Title { get; internal set; }
 
-		public Uri Link { get; set; }
+		public Uri Link { get; internal set; }
 
-		public string Description { get; set; }
+		public string Description { get; internal set; }
 
-		public DateTime ExportDate { get; set; }
+		public DateTime ExportDate { get; internal set; }
 
-		public string Language { get; set; }
+		public string Language { get; internal set; }
+
         public string ExportVersion { get; internal set; }
+
         public Uri BaseSiteUri { get; internal set; }
+
         public Uri BaseBlogUri { get; internal set; }
+
+        public List<Category> Categories { get; internal set; } = new List<Category>();
     }
 }
