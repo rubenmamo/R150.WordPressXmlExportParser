82e0427c4011408a94bd86770e361b96fb377f26
Ruben Mamo <ruben@4inline.com>
2023-06-05 07:26:37 +0200

    feat: Added support to extract author details (#9)

---
+++ src/WordpressXmlExportParser/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordPressXmlExportParser
+{
+    public struct Author
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
